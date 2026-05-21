fd0cb81706bf5347c037e026ebec367169871373
marcosqf93 <quintana.mqf@gmail.com>
2026-05-21 15:52:00 -0400

    Update User.js

--- admin/models/User.js
+++ admin/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true, lowercase: true, trim: true },
-    password: { type: String, required: true, minlength: 6 },
+    password: { type: String, required: true},
     avatar: { type: String, default: "" },
   },
   { timestamps: true }
